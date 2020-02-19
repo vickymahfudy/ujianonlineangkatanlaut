4c59525a2632dff471b3dce922547daa1b93f04f
vickymahfudy <vicky15@mhs.if.its.ac.id>
2020-02-19 16:30:30 +0700

    Update

--- export.sql
+++ export.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Feb 13, 2020 at 01:23 AM
+-- Generation Time: Feb 19, 2020 at 08:53 AM
 -- Server version: 5.7.26
 -- PHP Version: 7.3.8
 
@@ -23,6 +23,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `groupsoal` (
   `idgroup` int(11) NOT NULL,
   `namagroup` varchar(50) NOT NULL,
+  `idmapel` int(11) NOT NULL,
   `idgurumapel` int(11) NOT NULL,
   `statusgrup` enum('N','Y') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -31,9 +32,9 @@ CREATE TABLE `groupsoal` (
 -- Dumping data for table `groupsoal`
 --
 
-INSERT INTO `groupsoal` (`idgroup`, `namagroup`, `idgurumapel`, `statusgrup`) VALUES
-(8, 'percobaan', 2, 'N'),
-(9, 'percobaan 2', 4, 'Y');
+INSERT INTO `groupsoal` (`idgroup`, `namagroup`, `idmapel`, `idgurumapel`, `statusgrup`) VALUES
+(3, 'UAS - IPA', 2, 0, 'Y'),
+(4, 'UTS - Bahasa Indonesia', 1, 0, 'Y');
 
 -- --------------------------------------------------------
 
@@ -53,14 +54,8 @@ CREATE TABLE `gurumapel` (
 --
 
 INSERT INTO `gurumapel` (`idgurumapel`, `iduser`, `idmapel`, `idtahun`) VALUES
-(1, 24, 25, 1),
-(2, 24, 26, 1),
-(3, 26, 26, 1),
-(4, 32, 27, 1),
-(5, 24, 28, 1),
-(6, 26, 27, 1),
-(7, 32, 26, 1),
-(8, 32, 30, 2);
+(0, 0, 1, 1),
+(0, 0, 2, 1);
 
 -- --------------------------------------------------------
 
@@ -81,10 +76,8 @@ CREATE TABLE `jawaban` (
 --
 
 INSERT INTO `jawaban` (`idjawab`, `iduser`, `idujian`, `idsoal`, `jawaban`) VALUES
-(128, 25, 13, 8, 'a, b, c, d, e'),
-(129, 25, 13, 8, 'a, b, c, d, e'),
-(130, 25, 13, 5, 'a'),
-(131, 25, 13, 7, 'false');
+(4, 3, 1, 3, 'e'),
+(5, 3, 1, 1, 'a');
 
 -- --------------------------------------------------------
 
@@ -103,8 +96,8 @@ CREATE TABLE `mapel` (
 --
 
 INSERT INTO `mapel` (`idmapel`, `kdmapel`, `mapel`) VALUES
-(30, 'IG001', 'Fisika 1'),
-(31, 'IG002', 'Matematika 1');
+(1, 'bhs2020', 'Bahasa Indonesia'),
+(2, 'ipa2020', 'IPA');
 
 -- --------------------------------------------------------
 
@@ -125,7 +118,7 @@ CREATE TABLE `nilai` (
 --
 
 INSERT INTO `nilai` (`idnilai`, `iduser`, `idujian`, `nilai`, `tgl`) VALUES
-(51, 25, 13, 100, '2019-09-28');
+(3, 3, 1, 50, '2020-02-18');
 
 -- --------------------------------------------------------
 
@@ -148,8 +141,8 @@ CREATE TABLE `setujian` (
 --
 
 INSERT INTO `setujian` (`idujian`, `idgroup`, `waktu`, `token`, `status`, `rangesoal`, `user`) VALUES
-(13, 8, 100, 'specialone', 'aktif', 4, 24),
-(17, 8, 10, '100', 'aktif', 2, 26);
+(1, 4, 60, '12345', 'aktif', 2, 1),
+(4, 3, 10, '333', 'tidak aktif', 2, 1);
 
 -- --------------------------------------------------------
 
@@ -176,10 +169,8 @@ CREATE TABLE `soal` (
 --
 
 INSERT INTO `soal` (`idsoal`, `idgroup`, `jenissoal`, `soal`, `pilihana`, `pilihanb`, `pilihanc`, `pilihand`, `pilihane`, `pilihanbenar`, `pembahasan`) VALUES
-(5, 8, 'singlechoice', '<p>President pertama republik Indonesia ?</p>\r\n', '<p>Soekarno</p>\r\n', '<p>Bung Hatta</p>\r\n', '<p>Soeharto</p>\r\n', '<p>Habibie</p>\r\n', '<p>Jusuf Kalla</p>\r\n', 'a', '<p>jawaban yang benar adalah A</p>\r\n'),
-(7, 8, 'truefalse', '<p>soekarno adalah presiden ke 10</p>\r\n', '', '', '', '', '', 'false', '<p>salah, karena soekarno presiden ke 1</p>\r\n'),
-(8, 8, 'multipleanswer', '<p>berikut ini adalah presiden RI</p>\r\n', '<p>soekarno</p>\r\n', '<p>soeharto</p>\r\n', '<p>SBY</p>\r\n', '<p>Jokowi</p>\r\n', '<p>Gusdur</p>\r\n', 'a, b, c, d, e', '<p>semua jawaban benar</p>\r\n'),
-(9, 8, 'sorting', '<p>urutkan kejadian berikut</p>\r\n', 'berangkat sekolah', 'sarapan', 'berpakaian', 'mandi', 'Bangun tidur', 'e,d,c,b,a', '');
+(1, 4, 'singlechoice', '<p>1+1+1</p>\r\n', '<p>1</p>\r\n', '<p>2</p>\r\n', '<p>3</p>\r\n', '<p>4</p>\r\n', '<p>Semua benar</p>\r\n', 'c', '<p>1+1=2</p>\r\n'),
+(3, 4, 'singlechoice', '<p>5x5=</p>\r\n', '<p>25</p>\r\n', '<p>245</p>\r\n', '<p>343</p>\r\n', '<p>2342</p>\r\n', '<p>Tidak benar</p>\r\n', 'e', '<p>yeyeyeye</p>\r\n');
 
 -- --------------------------------------------------------
 
@@ -198,7 +189,7 @@ CREATE TABLE `tahunajaran` (
 --
 
 INSERT INTO `tahunajaran` (`idtahun`, `tahun`, `status`) VALUES
-(2, '2019 Genap', 'Y');
+(1, '2020', 'Y');
 
 -- --------------------------------------------------------
 
@@ -222,9 +213,9 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`iduser`, `namauser`, `jk`, `tempatlahir`, `tgllahir`, `status`, `username`, `password`) VALUES
-(1, 'admin', 'laki-laki', 'Negara', '2000-08-05', 'admin', 'admin', 'YWRtaW4='),
-(32, 'guru', 'laki-laki', 'Banyuwangi', '1997-01-05', 'guru', 'guru', 'Z3VydQ=='),
-(33, 'siswa', 'laki-laki', 'Banyuwangi', '1997-01-15', 'siswa', 'siswa', 'c2lzd2E=');
+(0, 'guru', 'laki-laki', '0', '2020-02-17', 'guru', '0', '0'),
+(2, 'admin', 'laki-laki', 'Negara', '1997-01-05', 'admin', 'admin', 'YWRtaW4='),
+(3, 'Vicky Mahfudy', 'laki-laki', 'Banyuwangi', '1997-01-05', 'siswa', 'vickym', 'dmlja3lt');
 
 --
 -- Indexes for dumped tables
@@ -235,12 +226,6 @@ INSERT INTO `user` (`iduser`, `namauser`, `jk`, `tempatlahir`, `tgllahir`, `stat
 --
 ALTER TABLE `groupsoal`
   ADD PRIMARY KEY (`idgroup`);
-
---
--- Indexes for table `gurumapel`
---
-ALTER TABLE `gurumapel`
-  ADD PRIMARY KEY (`idgurumapel`);
 
 --
 -- Indexes for table `jawaban`
@@ -293,52 +278,46 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `groupsoal`
 --
 ALTER TABLE `groupsoal`
-  MODIFY `idgroup` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
-
---
--- AUTO_INCREMENT for table `gurumapel`
---
-ALTER TABLE `gurumapel`
-  MODIFY `idgurumapel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `idgroup` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `jawaban`
 --
 ALTER TABLE `jawaban`
-  MODIFY `idjawab` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=132;
+  MODIFY `idjawab` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `mapel`
 --
 ALTER TABLE `mapel`
-  MODIFY `idmapel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `idmapel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `nilai`
 --
 ALTER TABLE `nilai`
-  MODIFY `idnilai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
+  MODIFY `idnilai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `setujian`
 --
 ALTER TABLE `setujian`
-  MODIFY `idujian` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `idujian` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `soal`
 --
 ALTER TABLE `soal`
-  MODIFY `idsoal` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `idsoal` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `tahunajaran`
 --
 ALTER TABLE `tahunajaran`
-  MODIFY `idtahun` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `idtahun` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `iduser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+  MODIFY `iduser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
--- home.php
+++ home.php
@@ -16,40 +16,6 @@
         <!-- Widget -->
         <div class="widget widget-hover-effect1">
             <div class="widget-simple">
-                <div class="widget-icon pull-left themed-background-default animation-fadeIn">
-                    <i class="fa fa-file-text"></i>
-                </div>
-                <h2 class="widget-content text-right">
-                    <strong><?php $tampil = mysqli_query($mysqli, "select count(namagroup) as jumlah from groupsoal");
-                    $hasil = mysqli_fetch_array($tampil);
-                    echo $hasil = $hasil['jumlah'];?></strong><br>
-                    <small>Group Soal</small>
-                </h2>
-            </div>
-        </div>
-        <!-- END Widget -->
-    </div>
-    <div class="col-sm-6 col-lg-3">
-        <!-- Widget -->
-        <div class="widget widget-hover-effect1">
-            <div class="widget-simple">
-                <div class="widget-icon pull-left themed-background-autumn animation-fadeIn">
-                    <i class="fa fa-user"></i>
-                </div>
-                <h3 class="widget-content text-right animation-pullDown">
-                    <strong><?php $tampil = mysqli_query($mysqli, "select count(namauser) as jumlah from user where status = 'guru' ");
-                    $hasil = mysqli_fetch_array($tampil);
-                    echo $hasil = $hasil['jumlah'];?></strong><br>
-                    <small>Guru</small>
-                </h3>
-            </div>
-        </div>
-        <!-- END Widget -->
-    </div>
-    <div class="col-sm-6 col-lg-3">
-        <!-- Widget -->
-        <div class="widget widget-hover-effect1">
-            <div class="widget-simple">
                 <div class="widget-icon pull-left themed-background-autumn animation-fadeIn">
                     <i class="fa fa-user"></i>
                 </div>
@@ -59,6 +25,23 @@
                     echo $hasil = $hasil['jumlah'];?></strong><br>
                     <small>Siswa</small>
                 </h3>
+            </div>
+        </div>
+        <!-- END Widget -->
+    </div>
+    <div class="col-sm-6 col-lg-3">
+        <!-- Widget -->
+        <div class="widget widget-hover-effect1">
+            <div class="widget-simple">
+                <div class="widget-icon pull-left themed-background-default animation-fadeIn">
+                    <i class="fa fa-file-text"></i>
+                </div>
+                <h2 class="widget-content text-right">
+                    <strong><?php $tampil = mysqli_query($mysqli, "select count(namagroup) as jumlah from groupsoal");
+                    $hasil = mysqli_fetch_array($tampil);
+                    echo $hasil = $hasil['jumlah'];?></strong><br>
+                    <small>Group Soal</small>
+                </h2>
             </div>
         </div>
         <!-- END Widget -->
